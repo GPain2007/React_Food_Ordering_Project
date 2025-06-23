2b9bd14d44cbcc39dd7f9f4711d0fe77e5a946ee
GenThrive <genthrive@ecorise.org>
2025-06-23 15:26:45 -0500

    added count to Header account

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
+import { useContext } from "react";
 import logoImg from "../assets/logo.jpg";
 import Buttons from "./UI/Buttons.jsx";
+import CartContext from "../store/CartContext.jsx";
 
 export default function Header() {
+  const cartContext = useContext(CartContext);
+  const totalItems = cartContext.items.reduce((total, item) => {
+    return total + item.quanity;
+  }, 0);
   return (
     <header id="main-header">
       <div id="title">
@@ -9,7 +15,7 @@ export default function Header() {
         <h1>React Food</h1>
       </div>
       <nav>
-        <Buttons textOnly> Cart (0)</Buttons>
+        <Buttons textOnly> Cart ({totalItems})</Buttons>
       </nav>
     </header>
   );
